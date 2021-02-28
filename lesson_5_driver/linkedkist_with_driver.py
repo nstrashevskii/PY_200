d44d04a46447d2fecaea792feb72c89f4fbc3fbd
nikolaj <strashevskiynn@g,ail.com>
2021-02-28 11:49:24 +0300

    linkedlist with driver задание

---
+++ lesson_5_driver/linkedkist_with_driver.py
@@ -0,0 +1,36 @@
+"""
+Реализовать класс LinkedListWithDriver от класса LinkedList.
+Дочерний класс должен уметь работать с реализованными ранее драйверами.
+    1. Реализовать свойство driver, которое будет возвращать используемый драйвер и проверять, что устанавливаемый
+        драйвер является экземпляром класса IStructureDriver.
+    2. Реализовать метод read, который с помощью встроенного драйвера будет получать последовательность элементов и
+        помещать их в самого себя. При вызове метода связанный список должен полностью перезаписываться новыми элементами.
+    3. Реализовать метод write, который передавать последовательность элементов для записи драйвером.
+    4. Протестировать паттерн "Стратегия" в ключе независимости работы экземляров LinkedListWithDriver от драйверов
+        IStructureDriver. LinkedListWithDriver должен уметь работать со всеми экземплярами дочерних классов класс IStructureDriver.
+    5. LinkedListWithDriver должен поддерживать "горячую замену" драйвера, то есть без удаления и создания нового
+        экземпляра LinkedListWithDriver, а замена драйвера существующего экземпляра.
+"""
+
+from py_class.LinketList import LinkedList
+from lesson_5_driver.driver import IStructureDriver, JsonFileDriver
+
+
+class LinkedListWithDriver(...):
+    def __init__(self, data, driver: IStructureDriver = None):
+        # ToDo вызвать конструктор базового класса LinkedList
+        ...
+        self.__driver = driver
+
+    def read(self):
+        """Взять драйвер и считать из него информацию в LinkedList"""
+        ...
+
+    def write(self):
+        """Взять драйвер и записать в него информацию из LinkedList"""
+        ...
+
+
+if __name__ == '__main__':
+    ll = LinkedListWithDriver([1, 2, 3, 4, 5])
+    ll.write()
